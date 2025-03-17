b4593ea0fa10672d9524ba2c5e2478b0557250e6
busrauludg <busrapc.226@gmail.com>
2025-03-17 17:44:31 +0300

    Veri tabanı bağlatısı

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();//bu bir serves sağlayıcısıdır boş bir ASP.NET Core projesi açtığımız için mvc desteğı aktif et diyoruz tanımı
+
+builder.Services.AddDbContext<RepositoryContext>(options=>
+{
+  options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
 
 var app = builder.Build();
 
