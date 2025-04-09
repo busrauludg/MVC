b1d86c0c8667b929f931275f3424e565a669136f
busrauludg <busrapc.226@gmail.com>
2025-04-09 18:22:44 +0300

    _Navbar
    Areada admin panelinde işlemler yaptık

--- Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e14650740674626ec5afad464901f2e7ce648ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ff14960ae91a58a7f7a411a5029b0b66d6ff52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
